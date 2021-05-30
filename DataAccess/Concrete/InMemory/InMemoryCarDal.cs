ce49713344ca4bf3e5f3bbd285d243ed065f1db1
Deniz <deniz_celik_846@hotmail.com>
2021-05-30 15:06:05 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal:ICarDal
+    {
+        private List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car {Id = 1, BrandId = 1, ColorId = 1, DailyPrice = 100, ModelYear = 2015, Description = "Good"},
+                new Car {Id = 2, BrandId = 1, ColorId = 3, DailyPrice = 200, ModelYear = 2016, Description = "Very Good"},
+                new Car {Id = 3, BrandId = 3, ColorId = 2, DailyPrice = 300, ModelYear = 2017, Description = "Not Bad"},
+                new Car {Id = 4, BrandId = 2, ColorId = 1, DailyPrice = 150, ModelYear = 2014, Description = "Good"},
+                new Car {Id = 5, BrandId = 1, ColorId = 2, DailyPrice = 145, ModelYear = 2019, Description = "Good"},
+            };
+        }
+
+        public Car GetById(int Id)
+        {
+            Car car;
+            car=_cars.SingleOrDefault(p => p.Id == Id);
+            return car;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate;
+            carToUpdate= _cars.SingleOrDefault(p => p.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.Description = car.Description;
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete;
+            carToDelete = _cars.SingleOrDefault(p => p.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+    }
+}
